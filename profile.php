8bb9dd73450d62710b2ebcdba176e01b127975bf
webunderground <101758687+webunderground@users.noreply.github.com>
2022-06-22 10:20:02 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -126,7 +126,7 @@ echo "The current date and time  $DateAndTime2.";
               <?php 
   // Se conecta al SGBD 
   $user=$_SESSION["username"]; 
-  if(!($conexion = mysql_connect("localhost", "root", "lolita1873"))) 
+  if(!($conexion = mysql_connect("localhost", "root", "password"))) 
     die("Error: No se pudo conectar");
  $user=$_SESSION['username'] ;
   // Selecciona la base de datos 
@@ -150,6 +150,9 @@ echo "The current date and time  $DateAndTime2.";
   
        
    echo "<div class='comment'><p>";
+	  echo"<span class='material-icons'> account_circle</span> ";	  
+
+	  
     echo "<a href='user.php?tag=" . $fila['usuario'] . "'>" . $fila['usuario'] . "</a><br/> ";
     echo $fila['text'] . '</p><br/>';
 	 echo $fila['email'] . '</p><br/>';
@@ -193,7 +196,7 @@ echo "The current date and time  $DateAndTime2.";
      
               <?php 
   // Se conecta al SGBD 
-  if(!($conexion = mysql_connect("localhost", "root", "lolita1873"))) 
+  if(!($conexion = mysql_connect("localhost", "root", "password"))) 
     die("Error: No se pudo conectar");
  
   // Selecciona la base de datos 
@@ -212,7 +215,9 @@ echo "The current date and time  $DateAndTime2.";
   { 
    echo "<div >";
  echo"
-      </div>";   
+      </div>"; 
+	  echo"<span class='material-icons'> account_circle</span> ";	  
+
    echo "<a href='" . $fila['website'] . "@'>" . $fila['usuario'] . "</a><br/> <div class='tiempo'>" . $fila['fecha'] . "</div>";
   
        
